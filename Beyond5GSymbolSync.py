ec4d771ef0ef5c3afc36720239954b3cd13101cb
JT Willard <willarj@purdue.edu>
2024-04-03 23:52:16 -0400

    Preamble Insertion Complete

--- Beyond5GSymbolSync.py
+++ Beyond5GSymbolSync.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random as r
 
 # Expected Preamble
 #WLAN = Enter WLAN preamble here
@@ -18,14 +19,28 @@ with open(bitstreamFile, 'r') as fid:
 
 bitstream = fid[0]
 bitstreamLen = len(bitstream)
+
 #--------------------------------------------------------------------------
 
+# Inserts the preamble somwhere randomly into the bitstream
+def preInsert(bits, bitLen, pre):
+    idx = r.randint(1, (bitLen - 2))
+    return(bits[0:idx] + pre + bits[(idx):(bitLen)])
+
 # Function to find optimal Accuracy Threshold
-def preambleBitstreamOptim():
-    print('Hello World')
+# 'bits' is the bitstream with the preamble inserted, 'pre' is the expected preamble
+def preambleBitstreamOptim(bits, pre):
+    print('Pre')
 
 # Function to find and report Data
-def findData():
+def findData(bits, pre):
     print('Data')
-    
-# Function to flip a certain percentage of the bits
+
+# Function to flip a certain percentage of the bits by subtracting each element by 1
+# and changing the -1 indexes to 0
+# 'bits' is the bitstream with the preamble inserted, 'percent' is the percent of bits to be flipped
+def bitFlip(bits, percent):
+    print('Flip')
+
+newBits = preInsert(bitstream, bitstreamLen, preamble)
+# percent = preambleBitstreamOptim(, preamble)
