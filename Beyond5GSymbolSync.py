9b0388bd595e446e4a1ba05b5e32194fd2b5d230
JT Willard <willarj@purdue.edu>
2024-04-24 14:26:01 -0400

    ?

--- Beyond5GSymbolSync.py
+++ Beyond5GSymbolSync.py
@@ -1,14 +1,11 @@
 import numpy as np
 import random as r
 import math as m
-import matplotlib.pyplot as plt
 
 
 # Expected Preamble
 WLAN = '01111110111011001110100010100100111110011001010110001100001001011110101010000010110101110010001110000000111100010000110100110110'
 Ethernet = '10101010101010101010101010101010101010101010101010101010'
-Fibonacci = '101100011111000000001111111111111000000000000000000000111111111111111111111111111111111100000000000000000000000000000000000000'
-Alternating = '101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010'
 
 # Preamble Manipulation
 preamble = WLAN
@@ -30,7 +27,7 @@ bitstreamLen = len(bitstream)
 # Inserts the preamble somwhere randomly into the bitstream
 def preInsert(bits, bitLen, pre):
     idx = r.randint(1, (bitLen - 2))
-    return(bits[0:idx] + pre + bits[(idx):(bitLen)], idx)
+    return(bits[0:idx] + pre + bits[(idx):(bitLen)])
 
 # Function to flip a certain percentage of the bits by subtracting each element by 1
 # and changing the -1 indexes to 0
@@ -60,7 +57,7 @@ def preambleBitstreamOptim(bits, pre):
     print('Pre')
 
 # Function to find and report Data for Troy
-def findData(bits, pre, threshold, expected):
+def findData(bits, pre, threshold):
     thresholdMet = False
     stream = []
     for bit in bits:
@@ -76,7 +73,7 @@ def findData(bits, pre, threshold, expected):
 
     nextIndex = preLength
     score = 100
-    while(nextIndex < bitstreamLen + preLength):
+    while(nextIndex < bitstreamLen):
         frame = np.delete(frame, 0)
         frame = np.append(frame, stream[nextIndex])
         differenceArr = np.abs(frame - preamble)
@@ -87,72 +84,36 @@ def findData(bits, pre, threshold, expected):
             thresholdMet = True
             break            
 
-    result = bits[nextIndex:]
     if(thresholdMet):
-        if(result == expected):
-            return 1
+        return bits[nextIndex:]
 
-    return -1   
-
+    return ("Threshold not met")   
 
 #-------------------------------------------------------------------------------------------------------------
 
 #Testing Section
 
-print("1. Locating the Preamble on a given Accuracy Threshold \n2. Exploring the relationship between Bits Flipped and accuracy at a given threshold")
+print("1. Locating the Preamble on a given Inaccuracy Threshold \n2. Exploring the relationship between Bits Flipped and accuracy at a given threshold")
 Decision = input("Enter the number corresponding to the desired function you want this program to run: ")
 
 if (Decision == '1'):
     Percent = int(input('Enter Desired Percentage of Bits Flipped (0 - 100): '))
-    newBits, index = preInsert(bitstream, bitstreamLen, preamble)   # Places the preamble in the bitstream
-    newBits = bitFlip(newBits, len(newBits), Percent)
-    threshold = int(input("Enter the Desired Accuracy Threshold (0-100): "))
-    inaccThreshold = 100 - threshold
+    newBits1 = preInsert(bitstream, bitstreamLen, preamble)   # Places the preamble in the bitstream
+    newBits2 = bitFlip(newBits1, len(newBits1), Percent)
+    threshold = input("Enter the Desired Inaccuracy Threshold (0-100): ")
     print("The following is the bitstream with the preamble inserted")
-    print(newBits)
+    print(newBits2)
     print("The following result is the outcome of the simulation:")
-    expected = newBits[(index + preLength):]
-    result = findData(newBits, preamble, inaccThreshold, expected)
-    if(result == 1):
-        print("Successful\nResult-")
-        print(expected)
-    else:
-        print("Unsuccessful, output not found correctly")
+    result = findData(newBits2, preamble, int(threshold))
+    print(result)
 
 elif (Decision == '2'):
-    threshold = int(input("Enter the Desired Accuracy Threshold (0-100): "))
-    x = []
-    y = []
-    inaccThreshold = 100 - threshold
-    upperBound = 100
-    lowerBound = 0
-    if inaccThreshold - 15 > 0:
-        lowerBound = inaccThreshold - 15
-
-    if inaccThreshold + 16 < 100:
-        upperBound = inaccThreshold + 16
-        
-    for noise in range(lowerBound, upperBound):
-        x.append(noise)
-        numCorrect = 0
-        for i in range(1000):
-            newBits, index = preInsert(bitstream, bitstreamLen, preamble)   # Places the preamble in the bitstream
-            newBits = bitFlip(newBits, len(newBits), noise)
-            expected = newBits[(index + preLength):]
-            if(findData(newBits, preamble, inaccThreshold, expected) == 1):
-                numCorrect += 1
-        y.append(numCorrect)
-        print("Num Correct:")
-        print(numCorrect)
-
-    plt.plot(x, y)
-
-    plt.xlabel('Noise Level')
-    plt.ylabel('Number of Correct Results')
-    plt.title('Correct Results vs. Noise Level')
-
-    plt.show()
+    threshold = input("Enter the Desired Accuracy Threshold (0-100): ")
     
 
 else:
     print("Decision selected was not one of the options")
+
+
+
+
